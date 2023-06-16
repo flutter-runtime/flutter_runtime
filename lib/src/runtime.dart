29d35ff957811b734a5b7b0bbb25a0b4a170ecd2
josercc <josercc@163.com>
2023-06-16 09:10:42 +0800

    新增 setField 方法可以设置属性 修改 call 方法支持返回 void 新增 packageName libraryPath 可以通过库名和路径进行查找

--- lib/src/runtime.dart
+++ lib/src/runtime.dart
@@ -1,15 +1,19 @@
-import 'dart:async';
-
 abstract class FlutterRuntime<T> {
   final T runtime;
   FlutterRuntime(this.runtime);
 
   // 获取当前类公开只读的属性
   dynamic getField(String fieldName);
+  void setField(String fieldName, dynamic value);
   // 执行当前类的公开方法
-  FutureOr call(String methodName, [Map args = const {}]);
+  dynamic call(String methodName, [Map args = const {}]);
   // 根据类名创建类实例
   dynamic createInstance(String className, [Map args = const {}]) {
     return null;
   }
+
+  // 当前归属的包名
+  String get packageName;
+  // 当前归属的库路径
+  String get libraryPath;
 }
