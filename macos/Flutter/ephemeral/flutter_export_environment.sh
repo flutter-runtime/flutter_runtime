fb09605037f7e240e7e6920baa5a7b942d6c26e4
张行 <3925@winnermedical.com>
2023-07-19 21:59:14 +0800

    no message

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/king/fvm/versions/stable"
-export "FLUTTER_APPLICATION_PATH=/Users/king/Documents/flutter_runtime"
+export "FLUTTER_ROOT=/Users/zhangxing/fvm/versions/3.7.12"
+export "FLUTTER_APPLICATION_PATH=/Users/zhangxing/Documents/flutter_runtime_ide/packages/flutter_runtime"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.0.1"
